e06628a06fb497ed7289e00badafd7a5f55dfb45
mubashirshayan <121729482+mubashirshayan@users.noreply.github.com>
2023-03-30 19:48:36 +0500

    new files

---
+++ app.js
@@ -0,0 +1,111 @@
+//Question no1
+//var arr=['',[]]
+//Question no5
+/*var arr=[[0,1,2,3,'<br>'],[1,0,1,2,'<br>'],[2,1,0,1,'<br>']]
+document.write(arr)*/
+
+//Question no3
+/*for (i=0;i<11;i++) {
+  document.write(i+'</br>')
+}*/
+
+
+//Question no4
+//example on table
+/*var inp=prompt('enter number')
+var inp2=prompt('enter length')
+for (var i=1;i<inp2;i++) {
+  document.write (inp+'×'+i+'='+inp*i+'</br>')
+
+}*/
+
+
+//Question no5
+
+/*var fruits=['apple','banana','mango','orange','strawberry']
+for (i=0;i<fruits.length;i++) {
+  document.write(fruits+'</br>')
+}*/
+//Question no6
+//Counting
+/*var count=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+document.write('<h3>Counting</h3>')
+for (var i=0;i<count.length+1; i++) {
+  document.write(i+',')
+}*/
+//Reverse counting 
+/*var revCount=[10,9,8,7,6,5,4,3,2,1]
+var temp=revCount.length
+document.write('<h3>Counting</h3>')
+/*revCount.reverse();
+console.log(revCount)*/
+/*for (var i = 10; i<10; i--) {
+  console.log(i)
+}*/
+//Even 
+/*var lent=10
+for (var i=1; i<=lent; i++) {
+  document.write(2*i+',')
+}*/
+//Series
+/*var lent=10
+for (var i=1; i<=lent; i++) {
+  document.write(2*i+'k'+',')
+}*/
+//Question no7
+/*var matchFound = false;
+var items=['cake', 'apple pie', 'cookie', 'chips', 'patties']
+var user=prompt('enter item name')
+for (var i=0 ;i<items.length;i++) {
+ if (user===items[i]) {
+   matchFound = true;
+    alert(items[i]+' '+'is available')
+   
+ }
+}
+if (matchFound === false) {
+ alert("It's not on the list");
+ }*/
+   // else{
+     // alert(user+'are not available')
+  //  }
+
+
+/*var matchFound = false;
+
+ for (var i = 0; i <= 4; i++){
+
+ if (cityToCheck === cleanestCities[i]) {
+ matchFound = true;
+alert("It's one of the cleanest cities");
+
+}
+}
+if (matchFound === false) {
+ alert("It's not on the list");
+ }*/
+//Question no8
+/*var num = [24, 53, 78, 91, 12]
+result = num[0]
+for (var i = 0; i < num.length; i++) {
+  if (num[i]>result) {
+    result = num[i]
+  }
+}
+
+console.log(result)*/
+
+//Question no9
+/*var num = [24,53,78,91,12]
+result=num[0]
+for (var i=0;i<num.length;i++) {
+  if (num[i]<result) {
+    result=num[i]
+  }
+}
+console.log(result)*/
+
+//Question no10
+/*for (i=1;i<101;i++) {
+  document.write(5*i+',')
+}*/
